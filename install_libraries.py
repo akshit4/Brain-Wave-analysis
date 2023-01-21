2a7d99673ae742a7ac676cd5818d8a647faebe10
Akshit Karande <44699945+akshit4@users.noreply.github.com>
2023-01-21 14:01:27 +0530

    Update install_libraries.py

--- install_libraries.py
+++ install_libraries.py
@@ -1,11 +1,1 @@
-import subprocess
-
-def install(package):
-    subprocess.check_call([f'pip install {package}'], shell=True)
-
-# List of libraries to be installed
-libraries = ['numpy', 'pandas', 'matplotlib', 'seaborn', 'scikit-learn', 'scipy', 'mne', 'gradio ', 'keras']
-
-# Loop through the list and install each library
-for library in libraries:
-    install(library)
+!pip install numpy pandas matplotlib seaborn scikit-learn scipy mne pyriemann keras
